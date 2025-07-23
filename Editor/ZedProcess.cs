fa11998c7a0ff1f731a047e19a461a836689683a
Ramon Gonzalez Fernandez <git@damastacoda.dev>
2025-07-22 22:33:46 -0400

    Add support for running zed in a nix devshell

--- Editor/ZedProcess.cs
+++ Editor/ZedProcess.cs
@@ -11,19 +11,37 @@ namespace UnityZed
 
         private readonly NPath m_ExecPath;
         private readonly NPath m_ProjectPath;
+        private readonly NPath m_FlakePath;
+        private readonly NPath m_NixPath;
 
         public ZedProcess(string execPath)
         {
             m_ExecPath = execPath;
             m_ProjectPath = new NPath(Application.dataPath).Parent;
+            m_FlakePath = m_ProjectPath.Combine("flake.nix");
+            m_NixPath = new NPath("/run/current-system/sw/bin/nix");
         }
 
         public bool OpenProject(string filePath = "", int line = -1, int column = -1)
         {
             sLogger.Log("OpenProject");
 
+            var execCalled = m_ExecPath;
+
+            var args = new StringBuilder();
+
+            // Add flags for running `nix develop` if a flake exists
+            if (m_FlakePath.FileExists() && m_NixPath.FileExists()) {
+                execCalled = m_NixPath;
+                args.Append("develop ");
+                args.Append($"\"{m_ProjectPath.ToString()}\"");
+                args.Append(" --command ");
+                args.Append($"\"{m_ExecPath.ToString()}\"");
+                args.Append(" -- ");
+            }
+
             // always add project path
-            var args = new StringBuilder($"\"{m_ProjectPath}\"");
+            args.Append($"\"{m_ProjectPath}\"");
 
             // if file path is provided, add it too
             if (!string.IsNullOrEmpty(filePath))
@@ -44,7 +62,7 @@ namespace UnityZed
                 }
             }
 
-            return CodeEditor.OSOpenFile(m_ExecPath.ToString(), args.ToString());
+            return CodeEditor.OSOpenFile(execCalled.ToString(), args.ToString());
         }
     }
 }
